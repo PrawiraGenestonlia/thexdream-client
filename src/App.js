ac7d4c1c65425cda4ed11d0f599f3d7c8ebc3a5d
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-15 12:45:24 +0800

    Adding a netlify build script

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Test</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+      <div class="bg-white rounded-lg p-6">
+        <img class="h-16 w-16 rounded-full mx-auto" src="avatar.jpg" alt='' />
+        <div class="text-center">
+          <h2 class="text-lg">Prawira Genestonlia</h2>
+          <div class="text-purple-500">Founder</div>
+          <div class="text-gray-600">praw0001@ntu.edu.sg</div>
+        </div>
+      </div>
+    </>
   );
 }
 
